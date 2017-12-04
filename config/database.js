dfc91c946db10ce0aee1d1a4eb5377428ab1ad70
scaryPudel <katarina.golenyova@student.spseke.sk>
2017-12-04 10:39:20 +0100

    Docasne zmeny v promo css a indexe

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://cload:password@localhost:27017/Cload' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://@localhost:27017/Cload' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
